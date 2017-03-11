fe96e73332da45c5ad6aeb6a8302efa0f26b6569
Lee Dongjin <dongjin@apache.org>
2017-03-11 20:17:59 +0900

    Titanic: 1. Update enum type implementations. 2. Remove useless implicit methods. 3. Change age to Option[Float].

--- src/main/scala/com/github/dongjinleekr/spark/dataset/Titanic.scala
+++ src/main/scala/com/github/dongjinleekr/spark/dataset/Titanic.scala
@@ -18,7 +18,7 @@ object Titanic {
       StructField("_pClass", IntegerType, false),
       StructField("name", StringType, false),
       StructField("_sex", StringType, false),
-      StructField("age", StringType, true),
+      StructField("age", FloatType, true),
       StructField("sibsp", IntegerType, false),
       StructField("parch", IntegerType, false),
       StructField("ticket", StringType, false),
@@ -30,95 +30,92 @@ object Titanic {
 
   // Enum types
   // Passenger Class: 1st, 2nd or 3rd class
-  sealed trait PClass
+  sealed trait PClass {
+    def value: Int
+  }
   object PClass {
     def apply(int: Int): PClass = {
       int match {
-        case 1 => FirstClass
-        case 2 => SecondClass
-        case 3 => ThirdClass
+        case FirstClass.value => FirstClass
+        case SecondClass.value => SecondClass
+        case ThirdClass.value => ThirdClass
         case _ => throw new IllegalArgumentException
       }
     }
 
-    def unapply(pClass: PClass): Int = {
-      pClass match {
-        case FirstClass => 1
-        case SecondClass => 2
-        case ThirdClass => 3
-      }
-    }
+    def unapply(pClass: PClass): Int = pClass.value
   }
-  final case object FirstClass extends PClass
-  final case object SecondClass extends PClass
-  final case object ThirdClass extends PClass
+  final case object FirstClass extends PClass {
+    val value = 1
+  }
+  final case object SecondClass extends PClass {
+    val value = 2
+  }
+  final case object ThirdClass extends PClass {
+    val value = 3
+  }
 
   // Sex: Male or Female
-  sealed trait Sex
+  sealed trait Sex {
+    def value: String
+  }
   object Sex {
     def apply(str: String) = {
       str match {
-        case "male" => Male
-        case "female" => Female
+        case Male.value => Male
+        case Female.value => Female
         case _ => throw new IllegalArgumentException
       }
     }
 
-    def unapply(sex: Sex): String = {
-      sex match {
-        case Male => "male"
-        case Female => "female"
-      }
-    }
+    def unapply(sex: Sex): String = sex.value
   }
-  final case object Male extends Sex
-  final case object Female extends Sex
+  final case object Male extends Sex {
+    val value = "male"
+  }
+  final case object Female extends Sex {
+    val value = "female"
+  }
 
   // Port of Embarkation
-  sealed trait Embarkation
+  sealed trait Embarkation {
+    def value: String
+  }
   object Embarkation {
     def apply(str: String) = {
       str match {
-        case "c" => Cherbourg
-        case "q" => Queenstown
-        case "s" => Southampton
+        case Cherbourg.value => Cherbourg
+        case Queenstown.value => Queenstown
+        case Southampton.value => Southampton
         case _ => Unknown
       }
     }
 
-    def unapply(embarkation: Embarkation): String = {
-      embarkation match {
-        case Cherbourg => "c"
-        case Queenstown => "q"
-        case Southampton => "s"
-        case Unknown => ""
-      }
-    }
+    def unapply(embarkation: Embarkation): String = embarkation.value
   }
-  final case object Cherbourg extends Embarkation
-  final case object Queenstown extends Embarkation
-  final case object Southampton extends Embarkation
-  final case object Unknown extends Embarkation
+  final case object Cherbourg extends Embarkation {
+    val value = "c"
+  }
+  final case object Queenstown extends Embarkation {
+    val value = "q"
+  }
+  final case object Southampton extends Embarkation {
+    val value = "s"
+  }
+  final case object Unknown extends Embarkation {
+    val value = ""
+  }
 
   object implicits {
-    implicit def boolToInt(o: Boolean) = o match {
-      case true => 1
-      case false => 0
-    }
-
-    implicit def intToBool(e: Int) = e match {
-      case 0 => false
-      case 1 => true
-    }
-
-    implicit val optionalString: Encoder[Option[String]] = ExpressionEncoder()
+    implicit val optionalFloat: Encoder[Option[Float]] = ExpressionEncoder()
+    implicit val optionalStringEncoder: Encoder[Option[String]] = ExpressionEncoder()
   }
 
   // Type for Business Logic: it provides some additional functionality and clarity.
   // The name and type of some of the members of this type is different from its schema:
   // isSurvived, age, cabin, embarked.
   case class Passenger(id: Int, survival: Boolean, _pClass: Int, name: String, _sex: String,
-                       age: String, sibsp: Int, parch: Int, ticket: String, fare: Float,
+                       age: Option[Float], sibsp: Int, parch: Int, ticket: String, fare: Float,
                        cabin: Option[String], _embarked: String) {
     def pClass = PClass(_pClass)
     def sex = Sex(_sex)
